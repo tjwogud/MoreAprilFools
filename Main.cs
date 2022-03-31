e082d0b3e693ca879c5aff9bb29f6133cf02e610
tjwogud <rlatjsal78@gmail.com>
2022-04-01 07:04:08 +0900

    초기 커밋.

---
+++ Main.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace MoreAprilFools
+{
+    public class Main
+    {
+        public static UnityModManager.ModEntry.ModLogger Logger;
+        public static Harmony harmony;
+        public static bool IsEnabled = false;
+        public static UnityModManager.ModEntry modEntry;
+
+        public static void Setup(UnityModManager.ModEntry modEntry)
+        {
+            Main.modEntry = modEntry;
+            Logger = modEntry.Logger;
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnUpdate = OnUpdate;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            IsEnabled = value;
+            if (value)
+            {
+                harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            else
+            {
+                harmony.UnpatchAll(modEntry.Info.Id);
+            }
+            return true;
+        }
+
+        private static void OnUpdate(UnityModManager.ModEntry modEntry, float f)
+        {
+            if (IsEnabled && !ADOBase.IsAprilFools())
+            {
+                Logger.Log("This Mod Is Only For April Fools!!!");
+                modEntry.Active = false;
+            }
+        }
+    }
+}
---
+++ UpdatePatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace MoreAprilFools
+{
+    [HarmonyPatch(typeof(scrCamera), "Update")]
+    public static class UpdatePatch1
+    {
+        public static void Postfix(scrCamera __instance)
+        {
+            if (!scnLevelSelect.instance)
+                __instance.camobj.transform.localEulerAngles = new Vector3(__instance.transform.localEulerAngles.x, __instance.transform.localEulerAngles.y, __instance.rot + 180);
+        }
+    }
+
+    [HarmonyPatch(typeof(scrVfxPlus), "Update")]
+    public static class UpdatePatch2
+    {
+        public static void Postfix(scrVfxPlus __instance, scrCamera ___cam)
+        {
+            if (!scnLevelSelect.instance)
+                ___cam.transform.rotation = Quaternion.Euler(0, 0, __instance.camAngle + 180);
+        }
+    }
+}
